c80ce5ec5786cd560020899b5b33cf753ad4f268
Numan Ahmed <mdnmnahmed@gmail.com>
2023-01-08 23:55:12 +0530

    Authenticate user data & save into store

---
+++ service_frontend/utils/authenticateUser.js
@@ -0,0 +1,26 @@
+import { getSession } from "next-auth/react";
+import { authConstants } from "../store/constants";
+
+export const authenticateUser = async ({ state, dispatch, replace }) => {
+    const authenticated = state.user.authenticated || false;
+    console.log('authenticated: ', authenticated);
+    if (!authenticated) {
+        dispatch({
+            type: authConstants.LOGIN_REQUEST
+        })
+        const session = await getSession();
+        console.log("session: ", session);
+        if (session?.token) {
+            dispatch({
+                type: authConstants.LOGIN_SUCCESS,
+                payload: session.token
+            });
+        } else {
+            dispatch({
+                type: authConstants.LOGIN_FAILURE,
+                payload: null
+            });
+            replace('/login');
+        }
+    }
+}
