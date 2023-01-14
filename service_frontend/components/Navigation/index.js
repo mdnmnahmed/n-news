13d7beade1345843657eade9151f684ce25de578
Numan Ahmed <mdnmnahmed@gmail.com>
2023-01-14 12:51:34 +0530

    Fixed console

--- service_frontend/components/Navigation/index.js
+++ service_frontend/components/Navigation/index.js
@@ -9,32 +9,32 @@ const Navigation = ({ navColor }) => {
     return (
         <>
             <header className="text-gray-400 body-font">
-                <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-                    <div class="flex items-center flex-shrink-0 text-white mr-6">
-                        <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M12.5 22.1c1.8-7.2 0.3-10.8 13.5-10.8 10.8 0 12.15 8.1 1.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 5-12.15-8.1-17.55-9.45-3.6-6.45-9.45"></path></svg>
-                        <span class="font-semibold text-xl tracking-tight">N-News</span>
+                <nav className="flex flex-wrap items-center justify-between p-6 bg-teal-500">
+                    <div className="flex items-center flex-shrink-0 mr-6 text-white">
+                        <img src="../images/n-news.svg" width="30" />
+                        <span className="ml-4 text-xl font-semibold tracking-tight">N-News</span>
                     </div>
-                    <div class="block lg:hidden">
-                        <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-                            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+                    <div className="block lg:hidden">
+                        <button className="flex items-center px-3 py-2 text-teal-200 border border-teal-400 rounded hover:text-white hover:border-white">
+                            <svg className="w-3 h-3 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
                         </button>
                     </div>
-                    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto hidden md:block">
-                        <div class="text-sm lg:flex-grow">
-                            <Link href={`/`} class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                    <div className="flex-grow hidden block w-full lg:flex lg:items-center lg:w-auto md:block">
+                        <div className="text-sm lg:flex-grow">
+                            <Link href={`/`} className="block mt-4 mr-4 text-teal-200 lg:inline-block lg:mt-0 hover:text-white">
                                 Home
                             </Link>
-                            <Link href={`/news`} class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                            <Link href={`/news`} className="block mt-4 mr-4 text-teal-200 lg:inline-block lg:mt-0 hover:text-white">
                                 News
                             </Link>
                         </div>
                         <div>
                             {state?.user?.authenticated ? (
-                                <Link href={`login`} class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
+                                <Link href={`login`} className="inline-block px-4 py-2 mt-4 text-sm leading-none text-white border border-white rounded hover:border-transparent hover:text-teal-500 hover:bg-white lg:mt-0">
                                     Logout
                                 </Link>
                             ) : (
-                                <Link href={`login`} class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
+                                    <Link href={`login`} className="inline-block px-4 py-2 mt-4 text-sm leading-none text-white border border-white rounded hover:border-transparent hover:text-teal-500 hover:bg-white lg:mt-0">
                                     Login
                                     </Link>
                             )}
